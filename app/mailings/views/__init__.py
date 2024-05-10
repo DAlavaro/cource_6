146c02cd7aac39082e1a7908718e18b0e98d3825
DAlavaro <59216962+DAlavaro@users.noreply.github.com>
2024-05-10 21:36:01 +0300

    Реализован CRUD для сообщений

--- app/mailings/views/__init__.py
+++ app/mailings/views/__init__.py
@@ -3,6 +3,12 @@ __all__ = [
     'ClientCreateView',
     'ClientUpdateView',
     'ClientDeleteView',
+
+    'MessageView',
+    'MessageCreateView',
+    'MessageUpdateView',
+    'MessageDeleteView',
 ]
 
 from app.mailings.views.client_view import ClientView, ClientCreateView, ClientUpdateView, ClientDeleteView
+from app.mailings.views.message_view import MessageView, MessageCreateView, MessageUpdateView, MessageDeleteView
